8e14840154d0294a1b1252ff41d95eb9024449ff
Zhi Deng <adengz@gmail.com>
2026-05-09 20:21:35 -0500

    chore: ensure both dev servers integration and full stack app launch via docker compose

--- backend/api/config.py
+++ backend/api/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-env_file = Path(__file__).parent.parent.parent / ".env.dev"
+env_file = Path(__file__).parent.parent / ".env"
 
 
 class Settings(BaseSettings):
